efa2023e036c7fc773e36e8cb7b4cafc4bd8eaa3
가인지 <gainge@gainjiui-MacBookPro.local>
2022-05-24 19:55:56 +0900

    complete publishing to category page, login page, detail page

--- routes/public/ctrl.public.js
+++ routes/public/ctrl.public.js
@@ -5,5 +5,25 @@ exports.get_main = async (req, res) => {
 }
 
 exports.get_category = async (req, res) => {
+  return res.render('category', {
 
+  })
+}
+
+exports.get_category_with_id = async (req, res) => {
+  return res.render('category', {
+
+  })
+}
+
+exports.get_login = async (req, res) => {
+  return res.render('login', {
+
+  })
+}
+
+exports.get_detail = async (req, res) => {
+  return res.render('detail', {
+    
+  })
 }
--- routes/public/index.js
+++ routes/public/index.js
@@ -4,6 +4,12 @@ const ctrl = require('./ctrl.public')
 
 router.get('/', ctrl.get_main)
 
-router.get('/category/:id', ctrl.get_category)
+router.get('/category', ctrl.get_category)
+
+router.get('/category/:id', ctrl.get_category_with_id)
+
+router.get('/login', ctrl.get_login)
+
+router.get('/detail/:id', ctrl.get_detail)
 
 module.exports = router
